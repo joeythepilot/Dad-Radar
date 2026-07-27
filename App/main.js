3345991e7d8827db4738f4c4cf2a144c90df91b8
Joey <cfijoey@gmail.com>
2026-07-26 20:34:09 -0400

    Add PRE-FLIGHT display state

--- App/main.js
+++ App/main.js
@@ -14,7 +14,7 @@ const etaValue = document.querySelector("#eta-value");
 const mapOrigin = document.querySelector("#map-origin");
 const mapDestination = document.querySelector("#map-destination");
 const aircraftMarker = document.querySelector(".aircraft-marker");
-const activeMode = "HOME";
+const activeMode = "PRE_FLIGHT";
 const flightBoard = document.querySelector(".flight-board");
 const mapPanel = document.querySelector(".map-panel");
 const instrumentPanel = document.querySelector(".instrument-panel");
@@ -24,6 +24,20 @@ const dadRadarModes = {
     status: "HOME",
     flight: null
   },
+PRE_FLIGHT: {
+  status: "PRE-FLIGHT",
+  flight: {
+    number: "AA 1234",
+    origin: "ORD",
+    destination: "AVL",
+    destinationCity: "ASHEVILLE",
+    airspeed: 0,
+    heading: 0,
+    altitude: 0,
+    progress: 0,
+    eta: "7:42 PM"
+  }
+},
 
   EN_ROUTE: {
     status: "ENROUTE",
